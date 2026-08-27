f56cc0683aaf4c0f8cb36cbf666ecb66c19222ae
CedricKeibeck <58707914+CedricKeibeck@users.noreply.github.com>
2026-08-27 16:14:43 +0200

    Update personal information in site-config.js

--- site-config.js
+++ site-config.js
@@ -3,26 +3,26 @@
   De rest van de website hoeft daarvoor niet gewijzigd te worden.
 */
 window.SITE_CONFIG = {
-  firstName: "[Voornaam]",
-  fullName: "[Jouw volledige naam]",
+  firstName: "[Cedric]",
+  fullName: "[Keibeck Cedric]",
 
   // Vervang assets/profile-placeholder.svg door bv. assets/profiel.jpg
   profileImage: "assets/profile-placeholder.svg",
 
-  education: "[Vul hier je exacte diploma's, universiteit(en) en eventueel huidige onderzoeksfunctie in]",
+  education: "[Master of Engineering: Energy -- KU Leuven ; Doctoraatstudent vermogenelektronica -- KU Leuven]",
 
   // Contact
-  email: "[jouw@email.be]",
-  phoneDisplay: "+32 ...",
-  phoneInternational: "+32...", // zonder spaties, bv. +32470123456
+  email: "[cedrickeibeck.bijles@gmail.com]",
+  phoneDisplay: "+32469255240",
+  phoneInternational: "+32469255240", // zonder spaties, bv. +32470123456
 
   // Tarief
-  priceLabel: "Tarief op aanvraag",
+  priceLabel: ".-€ 35",
   priceDetail: "Voor een individuele bijles van 60 minuten.",
 
   // Praktisch
-  travelText: "Ik geef bijles in Hasselt en omgeving en online. Pas hier je exacte regio en eventuele verplaatsingsvoorwaarden aan.",
-  cancellationText: "Kan een les niet doorgaan? Laat het zo vroeg mogelijk weten. Pas hier je definitieve annuleringsvoorwaarden aan.",
+  travelText: "Ik geef bijles in Hasselt en omgeving en online.",
+  cancellationText: "Kan een les niet doorgaan? Laat het zo vroeg mogelijk weten.",
 
   // Voeg alleen echte testimonials toe. Laat de lijst leeg zolang je er nog geen hebt.
   testimonials: [
