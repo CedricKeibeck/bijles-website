9461bda4257059348e51d90407bdccba601ed336
CedricKeibeck <58707914+CedricKeibeck@users.noreply.github.com>
2026-09-22 17:14:11 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+/* Publieke deployment-URL (GEEN studentensleutel en GEEN beheersleutel plaatsen). */
+window.BIJLES_PLANNER_APP_URL = 'https://script.google.com/macros/s/VERVANG_DOOR_JOUW_DEPLOYMENT_ID/exec';
